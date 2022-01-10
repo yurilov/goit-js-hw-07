94039fc07b124c61345f3a6f90e4ea7c9b75528b
yurilov <yuraschepilov@gmail.com>
2022-01-10 16:43:57 +0200

    started 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery(items) {
+  for (const item of items) {
+    const markup = `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`;
+
+    galleryRef.insertAdjacentHTML('beforeend', markup);
+  }
+}
+
+createGallery(galleryItems);
