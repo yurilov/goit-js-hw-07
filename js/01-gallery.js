35a199eeddb6613613c8c0591ca83432ea64b465
yurilov <yuraschepilov@gmail.com>
2022-01-10 15:24:57 +0200

    adding gallery to page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery(items) {
+  const galleryList = document.createElement('ul');
+  galleryList.classList.add('gallery__list');
+  
+
+  for (const item of items) {
+    const markup = `<li class = "gallery__item"><a class = "gallery__link"><img src = "${item.preview}" class = "gallery__image" data-source = "${item.original}" alt="${item.description}"></a></li>`;
+
+    galleryList.insertAdjacentHTML('beforeend', markup);
+  }
+
+  galleryRef.append(galleryList);
+}
+
+createGallery(galleryItems);
