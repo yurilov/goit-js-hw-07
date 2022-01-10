bcc63a2d6410dc5618cf3e8665c9c206956cc361
yurilov <yuraschepilov@gmail.com>
2022-01-10 15:35:04 +0200

    1 file modified

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,17 +3,20 @@ import { galleryItems } from './gallery-items.js';
 const galleryRef = document.querySelector('.gallery');
 
 function createGallery(items) {
-  const galleryList = document.createElement('ul');
-  galleryList.classList.add('gallery__list');
-  
-
   for (const item of items) {
-    const markup = `<li class = "gallery__item"><a class = "gallery__link"><img src = "${item.preview}" class = "gallery__image" data-source = "${item.original}" alt="${item.description}"></a></li>`;
+    const markup = `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`;
 
-    galleryList.insertAdjacentHTML('beforeend', markup);
+    galleryRef.insertAdjacentHTML('beforeend', markup);
   }
-
-  galleryRef.append(galleryList);
 }
 
 createGallery(galleryItems);
